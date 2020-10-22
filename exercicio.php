95efb2fbb88926f7b65bdcba2f9709dc0e6d14b7
Fernando Souza <fernandorr.fs@gmail.com>
2020-10-22 11:06:22 +0000

    Fist commit

---
+++ exercicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&family=Montserrat&family=Open+Sans&family=Roboto&family=Source+Sans+Pro&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicios.css">
+</head>
+<body class="exercicio">
+<header class="cabecalho">
+    <h1>Curso PHP</h1>
+    <h2>Visualização do exercício</h2>
+</header>
+<nav class="navegacao">
+    <a href="#" class="verde">Sem formatação</a>
+    <a href="index.php" class="vermelho">Voltar</a>
+</nav>
+<main class="principal">
+    <div class="conteudo">
+
+    </div>
+</main>
+<footer class="rodape">
+    COD3R E ALUNOS <?= date('Y'); ?>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&family=Montserrat&family=Open+Sans&family=Roboto&family=Source+Sans+Pro&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+</head>
+<body>
+<header class="cabecalho">
+    <h1>Curso PHP</h1>
+    <h2>índice dos exercícios</h2>
+</header>
+<main class="principal">
+    <div class="conteudo">
+        <nav class="modulos">
+            <div class="modulo vermelho">
+                <h3>Módulo 01</h3>
+                <ul>
+                    <li><a href="exercicio.php">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                    <li><a href="#">Exercício D</a></li>
+                </ul>
+            </div>
+            <div class="modulo azul">
+                <h3>Módulo 02</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                    <li><a href="#">Exercício D</a></li>
+                </ul>
+            </div>
+            <div class="modulo verde">
+                <h3>Módulo 03</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                    <li><a href="#">Exercício D</a></li>
+                </ul>
+            </div>
+            <div class="modulo cinza">
+                <h3>Módulo 04</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                    <li><a href="#">Exercício D</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</main>
+<footer class="rodape">
+    COD3R E ALUNOS <?= date('Y'); ?>
+</footer>
+</body>
+</html>
