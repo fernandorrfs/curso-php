910504757dbb7474c873493d868305647738ae93
Fernando Souza <fernandorr.fs@gmail.com>
2020-10-26 10:31:06 +0000

    equação

--- variaveis/basico.php
+++ variaveis/basico.php
@@ -1,3 +1,21 @@
 <div class="titulo">Variáveis</div>
 
 <?php
+$a = 30**2;
+$b = 6;
+
+$c = -4 * -5;
+$d = 2;
+
+$ab = $a / $b;
+$c2 = $c**2;
+$d2 = $d**2;
+
+$cd = $c2 / $d2;
+
+$sub = $ab - $cd;
+$sub3 = $sub**3;
+
+$final = $sub3 / (10**3);
+
+var_dump($final);
